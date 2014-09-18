6df569d0f2e6542ae663fe1cd8c0ff375efe8bc6
Marghoob Mohiyuddin <marghoobm@binatechnologies.com>
2014-09-18 13:18:40 -0700

    Add quickstart.sh which runs upto read-generation from the quickstart guide

---
+++ quickstart.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+# Download varsim
+wget https://github.com/bioinform/varsim/releases/download/0.2-beta/varsim-0.2-beta.tar.gz
+tar xfz varsim-0.2-beta.tar.gz
+
+# Download reference and variant databases 
+wget http://goo.gl/lgT18V
+wget http://web.stanford.edu/group/wonglab/varsim/insert_seq.txt
+wget http://web.stanford.edu/group/wonglab/varsim/GRCh37_hg19_supportingvariants_2013-07-23.txt
+wget http://goo.gl/NUG0dy
+gunzip hs37d5.fa.gz
+
+# Download samtools and index reference
+mkdir samtools
+cd samtools
+wget http://sourceforge.net/projects/samtools/files/samtools/1.0/samtools-bcftools-htslib-1.0_x64-linux.tar.bz2
+tar xfj samtools-bcftools-htslib-1.0_x64-linux.tar.bz2
+cd ..
+samtools/samtools-bcftools-htslib-1.0_x64-linux/bin/samtools faidx hs37d5.fa
+
+# Download ART
+mkdir ART
+cd ART
+wget http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114linux64tgz.tgz
+tar xfz artbinvanillaicecream031114linux64tgz.tgz
+cd ..
+
+# Test run varsim to generate 1x coverage data
+./varsim.py --vc_in_vcf All.vcf.gz --sv_insert_seq insert_seq.txt \
+--sv_dgv GRCh37_hg19_supportingvariants_2013-07-23.txt \
+--reference hs37d5.fa --id simu --read_length 100 --vc_num_snp 3000000 --vc_num_ins 100000 \
+--vc_num_del 100000 --vc_num_mnp 50000 --vc_num_complex 50000 --sv_num_ins 2000 \
+--sv_num_del 2000 --sv_num_dup 200 --sv_num_inv 1000 --sv_percent_novel 0.01 \
+--vc_percent_novel 0.01 --mean_fragment_size 350 --sd_fragment_size 50 \
+--vc_min_length_lim 0 --vc_max_length_lim 49 --sv_min_length_lim 50 \
+--sv_max_length_lim 1000000 --nlanes 3 --total_coverage 1 \
+--art ART/art_bin_VanillaIceCream/art_illumina --out_dir out --log_dir log --work_dir work \
+--simulator art
