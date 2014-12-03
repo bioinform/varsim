7a23cf7f100adf6bfe436a5c9b3d122308842258
John Mu <johnmu@umnhoj.com>
2014-12-03 13:56:44 -0800

    json inserter

---
+++ src/main/java/com/binatechnologies/varsim/JSONInserter.java
@@ -0,0 +1,20 @@
+package com.binatechnologies.varsim;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by johnmu on 12/3/14.
+ */
+public class JSONInserter {
+    private final static Logger log = Logger.getLogger(JSONInserter.class.getName());
+
+
+
+    public void run(String[] args){
+
+    }
+
+    public static void main(String[] args){
+        new JSONInserter().run(args);
+    }
+}
