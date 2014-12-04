bf078098018f78b0d9dadff85d8a9611fda0e2b2
John Mu <johnmu@umnhoj.com>
2014-12-03 16:39:29 -0800

    more json insertion code

--- src/main/java/com/binatechnologies/varsim/JSONInserter.java
+++ src/main/java/com/binatechnologies/varsim/JSONInserter.java
@@ -5,7 +5,10 @@ import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -17,10 +20,10 @@ public class JSONInserter {
     @Argument(usage = "One or more JSON files from VarSim",metaVar = "json_files ...",required = true)
     private ArrayList<String> json_filename = new ArrayList<>();
 
-    @Option(name = "-html", usage = "HTML to insert the JSON into" ,metaVar = "file",required = true)
-    String true_vcf_filename;
+    @Option(name = "-html", usage = "VarSim HTML to insert the JSON into" ,metaVar = "file",required = true)
+    File html_file;
 
-    public void run(String[] args){
+    public void run(String[] args) throws IOException{
         String VERSION = "VarSim " + getClass().getPackage().getImplementationVersion();
         String usage = "Inserts n JSON files to one HTML to create n HTML files\n";
 
@@ -33,15 +36,25 @@ public class JSONInserter {
         } catch (CmdLineException e) {
             System.err.println(VERSION);
             System.err.println(e.getMessage());
-            System.err.println("java -jar VarSim.jar json_inserter [options...] vcf_files ...");
+            System.err.println("java -jar VarSim.jar json_inserter [options...] json_files ...");
             // print the list of available options
             parser.printUsage(System.err);
             System.err.println(usage);
             return;
         }
+
+        String varsim_html = FileUtils.readFileToString(html_file);
+
+        for(String filename : json_filename){
+            // generate output_filename
+            String outName = html_file.getName() + filename.split(".")[0];
+            File file = new File(outName);
+
+            FileUtils.writeStringToFile(file, blah);
+        }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         new JSONInserter().run(args);
     }
 }
