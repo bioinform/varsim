808b0561b91e619903d8b279d883f507ec066e78
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-02-23 16:13:03 -0800

    improve BlockType for better code

--- src/main/java/com/bina/varsim/fastqLiftover/types/MapBlock.java
+++ src/main/java/com/bina/varsim/fastqLiftover/types/MapBlock.java
@@ -13,7 +13,7 @@ public class MapBlock implements Comparable<MapBlock> {
         srcLoc = new GenomeLocation(srcChr, srcLocation);
         dstLoc = new GenomeLocation(dstChr, dstLocation);
         this.direction = direction.equals("+") ? 0 : 1;
-        this.blockType = BlockType.valueOf(featureType);
+        this.blockType = BlockType.fromName(featureType);
         this.name = name;
     }
 
@@ -45,20 +45,39 @@ public class MapBlock implements Comparable<MapBlock> {
     }
 
     public enum BlockType {
-        SEQ("S"), INS("I"), DEL("D"), INV("V"), DUP_TANDEM("T"), UNKNOWN("U");
+        SEQ("S", "Sequence"), INS("I", "Insertion"), DEL("D", "Deletion"), INV("V", "Inversion"), DUP_TANDEM("T", "Tandem_Duplication"), UNKNOWN("U", "Unknown");
 
-        private final String name;
+        private final String shortName;
+        private final String longName;
 
-        private BlockType(String s) {
-            name = s;
+        BlockType(String shortName, String longName) {
+            this.shortName = shortName;
+            this.longName = longName;
+        }
+
+        public static BlockType fromName(final String s) {
+            for (final BlockType blockType : values()) {
+                if (blockType.name().equals(s) || blockType.shortName.equals(s) || blockType.longName.equals(s)) {
+                    return blockType;
+                }
+            }
+            return "".equals(s) ? SEQ : UNKNOWN;
         }
 
         public boolean equalsName(String otherName) {
-            return (otherName == null) ? false : name.equals(otherName);
+            return (otherName == null) ? false : shortName.equals(otherName);
         }
 
         public String toString() {
-            return name;
+            return shortName;
+        }
+
+        public String getShortName() {
+            return shortName;
+        }
+
+        public String getLongName() {
+            return longName;
         }
     }
 }
