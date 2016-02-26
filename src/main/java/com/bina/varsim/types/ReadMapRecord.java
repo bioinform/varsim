d9e80e14e28e4809be87380593dba9f312aef22f
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-02-25 17:37:29 -0800

    fix parsing and printing of readmaprecord

--- src/main/java/com/bina/varsim/types/ReadMapRecord.java
+++ src/main/java/com/bina/varsim/types/ReadMapRecord.java
@@ -1,38 +1,54 @@
 package com.bina.varsim.types;
 
-import com.bina.varsim.fastqLiftover.types.GenomeLocation;
+import com.bina.varsim.fastqLiftover.types.GenomeInterval;
 import com.google.common.base.Joiner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
-/**
- * Created by mohiyudm on 2/22/16.
- */
 public class ReadMapRecord {
+    public static final String COLUMN_SEPARATOR = "\t";
+    public static final String MAP_BLOCK_SEPARATOR = ":";
+    private static final Joiner joiner = Joiner.on(MAP_BLOCK_SEPARATOR).skipNulls();
+
     protected String readName;
-    private static final Joiner joiner = Joiner.on(",").skipNulls();
 
-    protected Collection<GenomeLocation> alignmentLocations;
+    protected List<Collection<ReadMapBlock>> multiReadMapBlocks;
 
-    public ReadMapRecord(final String readName, final Collection<GenomeLocation> alignmentLocations) {
+    public ReadMapRecord(final String readName, final List<Collection<ReadMapBlock>> multiReadMapBlocks) {
         this.readName = readName;
-        this.alignmentLocations = alignmentLocations;
+        this.multiReadMapBlocks = multiReadMapBlocks;
+    }
+
+    public ReadMapRecord(final String readName, final Collection<ReadMapBlock> ... multiReadMapBlocks) {
+        this.readName = readName;
+        this.multiReadMapBlocks = Arrays.asList(multiReadMapBlocks);
     }
 
     public ReadMapRecord(final String line) {
-        final String[] fields = line.split("\t");
+        final String[] fields = line.split(COLUMN_SEPARATOR);
         readName = fields[0];
-        alignmentLocations = new ArrayList<>();
-        final String locationStrings[] = fields[1].split(",", -1);
-        for (String locationString : locationStrings) {
-            if (!("".equals(locationString))) {
-                alignmentLocations.add(new GenomeLocation(locationString));
+        multiReadMapBlocks = new ArrayList<>();
+        for (int i = 1; i < fields.length; i++) {
+            final Collection<ReadMapBlock> readMapBlocks = new ArrayList<>();
+            final String readMapStrings[] = fields[i].split(MAP_BLOCK_SEPARATOR, -1);
+            for (final String readMapString : readMapStrings) {
+                readMapBlocks.add(new ReadMapBlock(readMapString));
             }
+            multiReadMapBlocks.add(readMapBlocks);
         }
     }
 
     public String toString() {
-        return readName + "\t" + joiner.join(alignmentLocations);
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(readName);
+        stringBuilder.append(COLUMN_SEPARATOR);
+        for (final Collection<ReadMapBlock> readMapBlocks : multiReadMapBlocks) {
+            stringBuilder.append(COLUMN_SEPARATOR);
+            stringBuilder.append(joiner.join(readMapBlocks));
+        }
+        return stringBuilder.toString();
     }
 }
