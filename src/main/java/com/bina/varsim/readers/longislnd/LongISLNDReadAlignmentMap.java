8132f20a5f81647131e9b6090c73d463c80674bd
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-02-29 14:26:59 -0800

    logging and add reading from multiple map files

--- src/main/java/com/bina/varsim/readers/longislnd/LongISLNDReadAlignmentMap.java
+++ src/main/java/com/bina/varsim/readers/longislnd/LongISLNDReadAlignmentMap.java
@@ -6,29 +6,32 @@ import htsjdk.tribble.CloseableTribbleIterator;
 import htsjdk.tribble.bed.BEDCodec;
 import htsjdk.tribble.bed.BEDFeature;
 import htsjdk.tribble.readers.LineIterator;
+import org.apache.log4j.Logger;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by mohiyudm on 2/22/16.
  */
 public class LongISLNDReadAlignmentMap {
+    private final static Logger log = Logger.getLogger(LongISLNDReadAlignmentMap.class.getName());
+
     final protected Map<String, ReadMapRecord> readAlignmentMap = new TreeMap<>();
 
-    public LongISLNDReadAlignmentMap(final File readAlignmentMapFile) throws IOException {
-        final AbstractFeatureReader<BEDFeature, LineIterator> featureReader = AbstractFeatureReader.getFeatureReader(readAlignmentMapFile.getName(), new BEDCodec(), false);
-        try {
-            final CloseableTribbleIterator<BEDFeature> featureIterator = featureReader.iterator();
-            while (featureIterator.hasNext()) {
-                final BEDFeature feature = featureIterator.next();
-                readAlignmentMap.put(feature.getName(), new LongISLNDReadMapRecord(feature).toReadMapRecord());
+    public LongISLNDReadAlignmentMap(final Collection<File> readAlignmentMapFiles) throws IOException {
+        for (final File readAlignmentMapFile : readAlignmentMapFiles) {
+            log.info("Reading in read map from " + readAlignmentMapFile.getName());
+            final AbstractFeatureReader<BEDFeature, LineIterator> featureReader = AbstractFeatureReader.getFeatureReader(readAlignmentMapFile.getName(), new BEDCodec(), false);
+            try {
+                final CloseableTribbleIterator<BEDFeature> featureIterator = featureReader.iterator();
+                while (featureIterator.hasNext()) {
+                    final BEDFeature feature = featureIterator.next();
+                    readAlignmentMap.put(feature.getName(), new LongISLNDReadMapRecord(feature).toReadMapRecord());
+                }
+            } finally {
+                featureReader.close();
             }
-        } finally {
-            featureReader.close();
         }
     }
 
