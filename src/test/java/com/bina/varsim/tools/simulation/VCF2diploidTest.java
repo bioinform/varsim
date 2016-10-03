7a381ff6b6f4c055c729eef973f207c1849c8ec2
Yunfei Guo <yunfei.guo@bina.roche.com>
2016-10-03 11:32:23 -0700

    add one mixed deletion + insertion test;

--- src/test/java/com/bina/varsim/tools/simulation/VCF2diploidTest.java
+++ src/test/java/com/bina/varsim/tools/simulation/VCF2diploidTest.java
@@ -180,4 +180,29 @@ public class VCF2diploidTest {
         assertTrue(FileUtils.contentEquals(outputPaternalReferencePath.toFile(), new File(paternalReference)));
         assertTrue(FileUtils.contentEquals(runner.getOutputMap(), new File(map)));
     }
+    @Test
+    public void MixedTest() throws IOException {
+        File wd = tmpFolder.newFolder("MixedTest");
+        String reference = "src/test/resources/MixedTest/oneMixedTest.fa";
+        String vcf = "src/test/resources/MixedTest/oneMixedTest.vcf";
+        String map = "src/test/resources/MixedTest/oneMixedTest.map";
+        String maternalReference = "src/test/resources/MixedTest/1_test_maternal.fa";
+        String paternalReference = "src/test/resources/MixedTest/1_test_paternal.fa";
+
+        Path outputVCFPath = Paths.get(wd.getCanonicalPath(), "1_test.vcf");
+        Path outputMaternalReferencePath = Paths.get(wd.getCanonicalPath(), "1_test_maternal.fa");
+        Path outputPaternalReferencePath = Paths.get(wd.getCanonicalPath(), "1_test_paternal.fa");
+
+        VCF2diploid runner = new VCF2diploid();
+        String[] args = new String[]{
+                "-chr", reference, "-outdir", wd.getCanonicalPath(),
+                "-seed", Integer.toString(this.seed), "-id", "test",
+                "-t", "MALE", "-vcf", vcf
+        };
+        runner.run(args);
+        assertTrue(FileUtils.contentEquals(outputVCFPath.toFile(), new File(vcf)));
+        assertTrue(FileUtils.contentEquals(outputMaternalReferencePath.toFile(), new File(maternalReference)));
+        assertTrue(FileUtils.contentEquals(outputPaternalReferencePath.toFile(), new File(paternalReference)));
+        assertTrue(FileUtils.contentEquals(runner.getOutputMap(), new File(map)));
+    }
 }
