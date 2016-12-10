d0fab27afbe9126e527a329faf5148c87f5449fa
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-12-09 17:36:12 -0800

    Trying to fix case madness

--- src/main/java/com/bina/varsim/tools/simulation/randVCFgenerator.java
+++ src/main/java/com/bina/varsim/tools/simulation/randVCFgenerator.java
@@ -151,7 +151,7 @@ abstract public class RandVCFgenerator {
         // the SV info is written here
         StringBuilder sbStr = new StringBuilder();
         // assume the SV type is consistent across alleles
-        if (var.getType() == VariantOverallType.TandemDup) {
+        if (var.getType() == VariantOverallType.Tandem_Duplication) {
             sbStr.append("SVTYPE=DUP;");
             sbStr.append("SVLEN=");
             sbStr.append(var.getLengthString());
