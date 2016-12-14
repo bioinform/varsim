17208e387ba90b84fb7a5bbbff3ef519bb094204
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-12-14 13:08:25 -0800

    Add tool classes

--- src/main/java/com/bina/varsim/VarSimToolNamespace.java
+++ src/main/java/com/bina/varsim/VarSimToolNamespace.java
@@ -1,25 +1,27 @@
 package com.bina.varsim;
 
 public enum VarSimToolNamespace {
-    VCF2Diploid("vcf2diploid", "Create a diploid genome as associated files from a reference genom and some VCF files."),
-    RandVCF2VCF("randvcf2vcf", "Randomly samples variants from VCF file."),
-    RandDGV2VCF("randdgv2vcf", "Randomly samples variants from DGV file."),
-    RandBED2VCF("randbed2vcf", "Generates a VCF file (to stdout) from an insertion and a deletion BED file. Insertions sequences are randomly sampled from the insert_seq file. This is designed for the Venter SV BED files."),
-    RandSequenceVCF("randsequencevcf", "Fill in missing insertion sequences by randomly sampling from a sequence file."),
-    VCFStats("vcfstats", "Get stats on variants from a VCF"),
-    VCFCompare("vcfcompare", "Generates a JSON with accuracy statistics of a VCF file relative to a truth"),
-    SAMCompare("samcompare", "Analyses the accuracy of the alignments in a SAM/BAM file. bed_file restricts the analysis to the bed regions"),
-    FastqLiftover("fastq_liftover", "Lift FASTQs to the right reference"),
-    LongISLNDLiftover("longislnd_liftover", "Lift read map files to the right reference"),
-    JSONInserter("json_inserter", "Inserts n JSON files to one HTML to create n HTML files"),
-    Unknown("", null);
+    VCF2Diploid("vcf2diploid", "Create a diploid genome as associated files from a reference genom and some VCF files.", com.bina.varsim.tools.simulation.VCF2diploid.class),
+    RandVCF2VCF("randvcf2vcf", "Randomly samples variants from VCF file.", com.bina.varsim.tools.simulation.RandVCF2VCF.class),
+    RandDGV2VCF("randdgv2vcf", "Randomly samples variants from DGV file.", com.bina.varsim.tools.simulation.RandDGV2VCF.class),
+    RandBED2VCF("randbed2vcf", "Generates a VCF file (to stdout) from an insertion and a deletion BED file. Insertions sequences are randomly sampled from the insert_seq file. This is designed for the Venter SV BED files.", com.bina.varsim.tools.simulation.RandBED2VCF.class),
+    RandSequenceVCF("randsequencevcf", "Fill in missing insertion sequences by randomly sampling from a sequence file.", com.bina.varsim.tools.simulation.RandSequenceVCF.class),
+    VCFStats("vcfstats", "Get stats on variants from a VCF", com.bina.varsim.tools.VCFstats.class),
+    VCFCompare("vcfcompare", "Generates a JSON with accuracy statistics of a VCF file relative to a truth", com.bina.varsim.tools.evaluation.VCFcompare.class),
+    SAMCompare("samcompare", "Analyses the accuracy of the alignments in a SAM/BAM file. bed_file restricts the analysis to the bed regions", com.bina.varsim.tools.evaluation.SAMcompare.class),
+    FastqLiftover("fastq_liftover", "Lift FASTQs to the right reference", com.bina.varsim.fastqLiftover.FastqLiftOver.class),
+    LongISLNDLiftover("longislnd_liftover", "Lift read map files to the right reference", com.bina.varsim.fastqLiftover.LongISLNDReadMapLiftOver.class),
+    JSONInserter("json_inserter", "Inserts n JSON files to one HTML to create n HTML files", com.bina.varsim.tools.evaluation.JSONInserter.class),
+    Unknown("", null, null);
 
     protected String command;
     protected String description;
+    Class toolClass;
 
-    VarSimToolNamespace(final String command, final String description) {
+    VarSimToolNamespace(final String command, final String description, final Class toolClass) {
         this.command = command;
         this.description = description;
+        this.toolClass = toolClass;
     }
 
     public static VarSimToolNamespace fromName(final String name) {
