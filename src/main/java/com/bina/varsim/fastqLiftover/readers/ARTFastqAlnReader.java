9f75017613f75d66f79b002e23aeb537823a2598
Yunfei Guo <yunfei.guo@bina.roche.com>
2017-01-23 15:21:23 -0800

    add logging

--- src/main/java/com/bina/varsim/fastqLiftover/readers/ARTFastqAlnReader.java
+++ src/main/java/com/bina/varsim/fastqLiftover/readers/ARTFastqAlnReader.java
@@ -6,42 +6,60 @@ import com.bina.varsim.fastqLiftover.types.SimulatedRead;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class ARTFastqAlnReader {
+    private final static Logger log = Logger.getLogger(ARTFastqAlnReader.class.getName());
     private final static Pattern nonACGTPattern = Pattern.compile("[^acgtACGT]");
     private BufferedReader fastqBr;
     private ArtAlnReader artAlnR;
     private boolean forceFiveBaseEncoding;
+    private long fqLineCount;
 
     public ARTFastqAlnReader(final BufferedReader alnR, final BufferedReader fastqBr, final boolean forceFiveBaseEncoding) throws IOException {
         this.fastqBr = fastqBr;
         this.artAlnR = new ArtAlnReader(alnR);
         this.forceFiveBaseEncoding = forceFiveBaseEncoding;
+        //assume no one has read from the above BufferedReaders
+        //this might not be true...
+        fqLineCount = 0;
     }
 
     public SimulatedRead getNextRead() throws IOException {
         SimulatedRead read;
         final String nameLine = fastqBr.readLine();
+        fqLineCount++;
         //TODO: add logging
-        if (nameLine == null || nameLine.trim().length() < 1) return null;
+        if (nameLine == null || nameLine.trim().length() < 1) {
+            if (nameLine.trim().length() < 1)
+                log.warning("got empty string at FASTQ line " + fqLineCount);
+            return null;
+        }
         ArtAlnRecord alnRecord;
-        if ((alnRecord = artAlnR.getNextAln()) == null) return null;
+        if ((alnRecord = artAlnR.getNextAln()) == null) {
+            return null;
+        }
 
         final String nameFields[] = nameLine.trim().substring(1).split("[-/]");
-        //TODO: add logging
-        if (nameFields.length < 2) return null;
+        if (nameFields.length < 2) {
+            log.warning("expect at least 2 fields at fastq line " + fqLineCount + ": " + nameLine);
+            return null;
+        }
         read = new SimulatedRead();
         read.fragment = Integer.parseInt(nameFields[nameFields.length - 1]);
         read.setReadId(nameFields[nameFields.length - 2]);
 
         read.sequence = fastqBr.readLine().trim();
+        fqLineCount++;
         if (forceFiveBaseEncoding) {
             read.sequence = nonACGTPattern.matcher(read.sequence).replaceAll("N");
         }
 
         fastqBr.readLine();
+        fqLineCount++;
         read.quality = fastqBr.readLine().trim();
+        fqLineCount++;
         if (read.fragment == 1) {
             read.locs1.add(new GenomeLocation(alnRecord.chromosome, alnRecord.location, alnRecord.direction));
             read.origLocs1.add(new GenomeLocation(alnRecord.chromosome, alnRecord.location, alnRecord.direction));
--- src/main/java/com/bina/varsim/fastqLiftover/readers/ArtAlnReader.java
+++ src/main/java/com/bina/varsim/fastqLiftover/readers/ArtAlnReader.java
@@ -7,16 +7,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class ArtAlnReader {
+    private final static Logger log = Logger.getLogger(ArtAlnReader.class.getName());
     private final static Pattern splitterPattern = Pattern.compile("[\\s]+");
     private BufferedReader br;
     private String currentLine;
     private Map<ChrString, Integer> chromosomeLengths;
+    private long alnLineCount;
 
     public ArtAlnReader(final BufferedReader br) throws IOException {
         this.br = br;
+        //assume reader is at file beginning
+        alnLineCount = 0;
 
         chromosomeLengths = new HashMap<>();
         readHeader();
@@ -24,6 +29,7 @@ public class ArtAlnReader {
 
     private void readHeader() throws IOException {
         while ((currentLine = br.readLine()) != null) {
+            alnLineCount++;
             if (!currentLine.startsWith("#") && !currentLine.startsWith("@")) {
                 break;
             }
@@ -40,14 +46,16 @@ public class ArtAlnReader {
         }
 
         final String refAln = br.readLine().trim();
+        alnLineCount++;
         final String readAln = br.readLine().trim();
+        alnLineCount++;
 
         final String[] alnFields = splitterPattern.split(currentLine.trim().substring(1));
         int direction = (alnFields[3].equals("-")) ? 1 : 0;
         final int refLength = refAln.replace("-", "").length();
         ArtAlnRecord record = new ArtAlnRecord(new ChrString(alnFields[0]), Integer.parseInt(alnFields[2]) + 1, direction, alnFields[1], refLength);
         if (!chromosomeLengths.containsKey(record.chromosome)) {
-            //TODO: add logging
+            log.warning("got nonexistent chromosome " + record.chromosome + " at aln file line " + alnLineCount);
             return null;
         }
         if (record.direction == 1) {
@@ -56,6 +64,7 @@ public class ArtAlnReader {
         }
 
         currentLine = br.readLine();
+        alnLineCount++;
         return record;
     }
 
