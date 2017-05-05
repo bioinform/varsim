35258449d5cc7c8723e836d8775756d7207be476
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2017-05-05 14:42:20 -0700

    Make .py script also usable as a package

--- build_release.sh
+++ build_release.sh
@@ -6,4 +6,4 @@ mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$version
 
 mvn package
 
-tar -cv quickstart.sh VarSim.jar liftover_restricted_vcf_map.py generate_small_test_ref.py varsim.py varsim_somatic.py | gzip > varsim-$version.tar.gz
+tar -cv quickstart.sh VarSim.jar __init__.py liftover_restricted_vcf_map.py generate_small_test_ref.py varsim.py varsim_somatic.py | gzip > varsim-$version.tar.gz
