2d3e7453ac5dc1dddc8d10b7a2fb9bd989a46945
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2017-05-22 16:32:09 -0700

    Changes to also handle overlapping regions

--- varsim_multi.py
+++ varsim_multi.py
@@ -15,7 +15,8 @@ import re
 from distutils.version import LooseVersion
 from liftover_restricted_vcf_map import lift_vcfs, lift_maps
 from generate_small_test_ref import gen_restricted_ref_and_vcfs 
-from varsim import varsim_main, get_version, check_java, get_loglevel, makedirs, RandVCFOptions, RandDGVOptions  
+from varsim import varsim_main, get_version, check_java, get_loglevel, makedirs, RandVCFOptions, RandDGVOptions, run_randvcf
+import pybedtools
 
 MY_DIR = os.path.dirname(os.path.realpath(__file__))
 VARSIMJAR = os.path.realpath(os.path.join(MY_DIR, "VarSim.jar"))
@@ -48,22 +49,47 @@ def varsim_multi(reference,
                  disable_vcf2diploid=False):
     logger = logging.getLogger(varsim_multi.__name__)
 
+    makedirs([out_dir])
+
     restricted_dir = os.path.join(out_dir, "restricted")
 
     restricted_reference, restricted_vcfs = gen_restricted_ref_and_vcfs(reference, variant_vcfs, regions, samples, restricted_dir, flank=0, short_contig_names=False)
-    _, [restricted_sampling_vcf] = gen_restricted_ref_and_vcfs(reference, [sampling_vcf], regions, [], os.path.join(out_dir, "restricted_sampling"), flank=0, short_contig_names=False)
+
+    merged_bedtool = pybedtools.BedTool(regions).merge() if regions else None
+
+    if regions and sampling_vcf:
+        merged_bed = os.path.join(out_dir, "merged.bed")
+        merged_bedtool = pybedtools.BedTool(regions).merge().saveas(merged_bed)
+        _, [restricted_sampling_vcf] = gen_restricted_ref_and_vcfs(reference, [sampling_vcf], merged_bed, [], os.path.join(out_dir, "region_restricted"), flank=0)
+        # Now lift over the restricted_sampling_vcf to get the region-limited VCF
+        sampling_vcf = lift_vcfs([restricted_sampling_vcf], os.path.join(out_dir, "region_restricted", "region-restricted-sampling.vcf"), reference)
+        
+
+    #_, [restricted_sampling_vcf] = gen_restricted_ref_and_vcfs(reference, [sampling_vcf], regions, [], os.path.join(out_dir, "restricted_sampling"), flank=0, short_contig_names=False)
 
     for index, sample in enumerate(samples):
         sample_dir = os.path.join(out_dir, sample)
         logger.info("Simulating sample {} in {}".format(sample, sample_dir))
+
+        # Run RandVCF first to get the sampled variants for the sample
+        if randvcf_options:
+            sampled_vcf = os.path.join(sample_dir, "randvcf.vcf")
+            with open(sampled_vcf, "w") as randvcf_out, open(os.path.join(sample_dir, "randvcf.err"), "w") as randvcf_log:
+                run_randvcf(sampling_vcf, randvcf_out, randvcf_log, seed + 1000 * index).wait()
+            # Now generate the restricted sampled VCF for the sample
+            _, [restricted_sampled_vcf] = gen_restricted_ref_and_vcfs(reference, [sampled_vcf], regions, [], os.path.join(sample_dir, "restricted_randvcf.vcf"), flank=0)
+            sample_variant_vcfs = restricted_vcfs + [restricted_sampled_vcf]
+        else:
+            sample_variant_vcfs = restricted_vcfs
+
         varsim_main(restricted_reference,
                     simulator,
                     simulator_exe,
                     total_coverage,
                     restricted_vcfs,
-                    restricted_sampling_vcf,
+                    None,
                     dgv_file,
-                    randvcf_options,
+                    None,
                     randdgv_options,
                     nlanes,
                     simulator_options,
