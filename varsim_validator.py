ddb4abd74f5729ea3f835470d6e931612509f5b5
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2017-05-26 17:50:05 -0700

    Handle multiple variants and simulation directories

--- varsim_validator.py
+++ varsim_validator.py
@@ -5,12 +5,9 @@ import os
 import sys
 import subprocess
 import logging
-import shutil
 import time
-import signal
 import itertools
 import glob
-import tempfile
 import re
 from distutils.version import LooseVersion
 from liftover_restricted_vcf_map import lift_vcfs, lift_maps
@@ -55,7 +52,7 @@ def aggregate_reports(sample_reports, samples, variant_type="all"):
                 continue
             summary_report = sum_counts(summary_report, sample_s[v]["sum_count"])
             if variant_type != "all":
-                summary_report = sum_counts(summary_report, sample_s[v]["sum_per_base_count"], keys_for_summation=["_TN", "tn"])
+                summary_report = sum_counts(summary_report, sample_s[v]["sum_per_base_count"], keys_for_summation=["tn"])
         if variant_type == "all":
             summary_report = sum_counts(summary_report, sample_reports[sample]["num_true_correct"]["all_data"]["sum_per_base_count"], keys_for_summation=["tn"])
         
@@ -76,7 +73,7 @@ def get_quantile(values, quantile=50.0):
     return sorted_values[int(len(values)*(1 - quantile/100.0))]
 
 
-def varsim_multi_validation(reference, regions, samples, varsim_dir, variants_dir, out_dir, vcfcompare_options="", disable_vcfcompare=False):
+def varsim_multi_validation(reference, regions, samples, varsim_dirs, variants_dirs, out_dir, vcfcompare_options="", disable_vcfcompare=False):
     logger = logging.getLogger(varsim_multi_validation.__name__)
 
     bed_options = "-bed {}".format(regions) if regions else ""
@@ -86,12 +83,17 @@ def varsim_multi_validation(reference, regions, samples, varsim_dir, variants_di
    
     samples_found = {}
     for sample in samples:
+        truth = filter(os.path.isfile, map(lambda d: os.path.join(d, sample, "out", "lifted", "truth.vcf"), varsim_dirs))
+        called = filter(os.path.isfile, map(lambda d: os.path.join(d, sample, "{}.vcf".format(sample)), variants_dirs))
+        if truth and called:
+            samples_found[sample] = {"truth": truth[0], "called": called[0]}
+
+    for sample in sorted(samples_found.keys()):
         sample_dir = os.path.join(out_dir, sample)
         makedirs([sample_dir])
 
-        sample_truth = os.path.join(varsim_dir, sample, "out", "lifted", "truth.vcf")
-        sample_called = os.path.join(variants_dir, sample, "{}.vcf".format(sample))
-        #sample_called = os.path.join(variants_dir, sample, "fb_0.35.vcf")
+        sample_truth = samples_found[sample]["truth"]
+        sample_called = samples_found[sample]["called"]
 
         if not os.path.isfile(sample_called):
             logger.error("{} missing".format(sample_called))
@@ -109,12 +111,10 @@ def varsim_multi_validation(reference, regions, samples, varsim_dir, variants_di
             logger.error("Accuracy report {} missing for {}".format(report_json, sample))
             continue
         
-        samples_found[sample] = {"truth": sample_truth, "called": sample_called}
         with open(report_json) as report_json_fd:
             sample_reports[sample] = json.load(report_json_fd)
 
         logger.info("Generated accuracy report for {}".format(sample))
-        #logger.info(json.dumps(sample_reports[sample]["num_true_correct"]["all_data"]["sum_count"], indent=2))
 
     # Now generate summary stats
 
@@ -157,8 +157,8 @@ if __name__ == "__main__":
     parser.add_argument("--reference", metavar="FASTA", help="Reference genome that variants will be inserted into", required=True)
     parser.add_argument("--regions", help="Regions of interest for simulation. Skip for whole genome simulation")
     parser.add_argument("--samples", help="Samples to be simulated", required=True, nargs="+")
-    parser.add_argument("--varsim", help="Root directory of multi-sample truth generation", required=True)
-    parser.add_argument("--variants", help="Root directory of variant calls", required=True)
+    parser.add_argument("--varsim", help="Root directory of multi-sample truth generation", nargs="+", required=True)
+    parser.add_argument("--variants", help="Root directory of variant calls", required=True, nargs="+")
     parser.add_argument("--vcfcompare_options", help="Other VCFCompare options", default="")
     parser.add_argument("--disable_vcfcompare", action="store_true", help="Do not run VCFcompare if already ran")
     parser.add_argument('--version', action='version', version=get_version())
