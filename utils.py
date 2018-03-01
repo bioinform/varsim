5d1493b29028c8b0f72be7e4ed85ea00f6cd8ac3
Yunfei Guo <guoyunfei1989@gmail.com>
2018-02-28 17:52:06 -0800

    rm simulator-specific options (for simpler interface)

--- utils.py
+++ utils.py
@@ -26,3 +26,25 @@ def versatile_open(filename, mode):
         return gzip.open(filename, mode)
     else:
         return open(filename, mode)
+
+def check_simulator_opts(simulator, simulator_opts):
+    '''
+    check opts string for a particular simulator
+    :param simulator:
+    :param opts:
+    :return:
+    '''
+    required_opts = {
+        dwgsim : ['-e', '-E', '-d', '-s', '-1', '-2'],
+        art : ['-1','-2','-p','-l','-m','-s'],
+        longislnd_required_opts : ['--fasta']
+    }
+    if simulator and simulator_opts:
+        if simulator in required_opts:
+            for i in required_opts[simulator]:
+                if not (i in simulator_opts):
+                    raise ValueError('{0} is missing for {1}'.format(i, simulator))
+        else:
+            raise NotImplementedError("simulator {0} is not supported".format(simulator))
+    else:
+        return
