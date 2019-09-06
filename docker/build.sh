a86246a7860e1fe5c139a6f248b8e9b3ad957ea6
Yunfei Guo <yunfei.guo@roche.com>
2019-09-05 19:03:12 -0700

    make build aware of branch

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 #build image and push
 version=$(git describe | sed 's/^v//')
-docker build --no-cache -f Dockerfile -t carlocreator/varsim:${version} .
-docker build --no-cache -f Dockerfile -t carlocreator/varsim:latest .
+branch=$(git rev-parse --abbrev-ref HEAD)
+docker build --no-cache --build-arg branch_of_interest=${branch} -f Dockerfile -t carlocreator/varsim:${version} .
+docker build --no-cache --build-arg branch_of_interest=${branch} -f Dockerfile -t carlocreator/varsim:latest .
 docker push carlocreator/varsim:${version}
 docker push carlocreator/varsim:latest
